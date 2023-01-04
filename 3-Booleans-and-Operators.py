f37ab31b6dba46579173f673c6afc8541b285263
Aaryash <31902160+4aryash@users.noreply.github.com>
2023-01-04 11:04:13 -0500

    04-01-2023

---
+++ 3-Booleans-and-Operators.py
@@ -0,0 +1,23 @@
+#!bin/python3
+
+valid= True
+not_valid= False
+
+print(valid, not_valid)		#printing the values of valid and not-valid
+
+print(valid==True, not_valid==False)	#checking conditions are met or not
+
+print(not valid, not not_valid)		#not keyword or ! give the same output
+
+print(10>5 or 10<5)	#or condition
+print(10>5 and 10<5)	#and condition
+
+print("\n----------------------------")
+print("|	PAGE DIVISION	   |")
+print("----------------------------\n")
+
+print(10 / 3)	#float output
+print(10 // 3)	#integer output
+print(10 % 3)	#modulus operand, gives remainder as output
+print(10 * 10)	#multiplication
+print(10 ** 10)	#10 raised to 10
