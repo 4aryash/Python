e7ff5f17b8449fb57eb06bb2a5903ef8fc99ac73
Aaryash <31902160+4aryash@users.noreply.github.com>
2023-01-12 22:39:32 -0500

    Create 4-Tuples.py

---
+++ 4-Tuples.py
@@ -0,0 +1,10 @@
+#!bin/python3
+
+tuple= ("Hello","World","I","am","KRK")	#creating a tuple of strings as its elements.
+
+for i in range(0,5):		#a loop of 5 iterations
+	#print(tuple)		#prints the entire tuple 5 times
+	print(tuple[i])		#This is how to print the elements inside a tuple.
+	
+tuple_repeat= ("Yo",)*5		#makes a tuple with 5 elements of Yo
+print(tuple_repeat) 
