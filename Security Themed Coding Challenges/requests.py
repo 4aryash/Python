c36999b43f22729be6db175cdb2d6fd6a6d8815c
Aaryash <31902160+4aryash@users.noreply.github.com>
2024-04-23 12:52:37 -0400

    Update requests.py

--- Security Themed Coding Challenges/requests.py
+++ Security Themed Coding Challenges/requests.py
@@ -2,6 +2,7 @@ import requests
 
 response = requests.get('http://crabby-clicker.ctf.umasscybersec.org/click')
 print(response.text)
+print(response.headers)
 print(response.status_code)
 
 response = requests.post('http://crabby-clicker.ctf.umasscybersec.org/click', data = {'hello':'world'})
@@ -9,3 +10,4 @@ if response.status_code == 200:
 	print('Message sent successfully!')
 else:
 	print('Message undelivered. Error code:', response.status_code)
+	#print(r'66.249.67.197 - - [18/Jul/2011:03:35:52 -0500] "GET /robots.txt HTTP/1.1" 404 286 "-" "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"')
