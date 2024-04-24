377bfe4af3d4b00ae9a31784d3a81505d36274b7
Aaryash <31902160+4aryash@users.noreply.github.com>
2024-04-24 01:10:38 -0400

    Create Rotate_string.py

---
+++ Leetcode/Rotate_string.py
@@ -0,0 +1,18 @@
+# s = "abcde"
+# goal = "cdeab"
+
+# list1 = list(s)
+# list2 = list(goal)
+
+# for i in range(1, len(list1)-1):
+# 	test_list = list1[i:] + list1[:i]
+#   print(test_list)
+# 	if test_list == list2:
+# 		print(True)
+# 	else:
+# 		print(False)
+
+
+# print(list1)
+# print(list2)
+# print(test_list)
