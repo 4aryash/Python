c3a40e8b062b2b9d9ef0d88075b2316f42146404
Robinbanksbyclickingkeys <31902160+4aryash@users.noreply.github.com>
2024-06-05 00:04:54 -0400

    Create Valid-Parentheses.py

---
+++ Leetcode/Valid-Parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        charmap = { ")":"(", "]":"[", "}":"{" }
+
+        for char in s:
+            if char in charmap:
+                if stack and stack[-1] == charmap[char]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(char)
+        
+        return True if not stack else False
