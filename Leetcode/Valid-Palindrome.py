ad8c687eb7d49b6a4f08edf3eb13de330158db06
Robinbanksbyclickingkeys <31902160+4aryash@users.noreply.github.com>
2024-06-18 12:41:24 -0400

    Update Valid-Palindrome.py

--- Leetcode/Valid-Palindrome.py
+++ Leetcode/Valid-Palindrome.py
@@ -1,12 +1,29 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        # stack = []
+        # result = ''
+        # for i in s:
+        #     if i.isalnum():
+        #         stack.append(i.lower())
+
+        # temp = stack
+        # stack = stack[::-1]
+        # result = ''.join(stack)
+        # return (temp == stack)
+
+        ### Using 2 pointers
         stack = []
-        result = ''
         for i in s:
             if i.isalnum():
                 stack.append(i.lower())
-
-        temp = stack
-        stack = stack[::-1]
-        result = ''.join(stack)
-        return (temp == stack)
+        left, right = 0, len(stack) - 1
+        while left <= right:
+            if stack[left] == stack[right]:
+                left += 1
+                right -= 1
+                if left == right:
+                    break
+            else:
+                return False
+        return True
+                
